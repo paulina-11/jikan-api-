cc4a19e5d6331d0eb6344f8e453d79d79c0eea43
paulina avila <paulina.avila@ids.com.mx>
2024-01-09 11:19:39 -0600

    actualizacion gsap

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -15,52 +15,64 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  // @ViewChild('bg')
-  // bg!: ElementRef<HTMLDivElement>;
-  // ngAfterViewInit() {
-  //   gsap.to(this.bg, {
-  //     ScrollTrigger: {
-  //       trigger: '#bg',
-  //       scrub: 1,
-  //     },
-  //     scale: 1.5,
-  //   });
-  // }
-
   constructor(private renderer: Renderer2) {}
 
-  @ViewChild('bg', { static: false })
-  bg!: ElementRef<HTMLImageElement>;
+  // @ViewChild('bg', { static: false })
+  // bg!: ElementRef<HTMLImageElement>;
 
   ngOnInit() {
     gsap.registerPlugin(ScrollTrigger);
-    this.scrollAnimations();
-  }
-
-  scrollAnimations() {
-    gsap.to(document.getElementById('bg'), {
+    // this.scrollAnimations();
+    gsap.to('#bg', {
       scrollTrigger: {
         scrub: 1,
       },
       scale: 1.5,
     });
-    gsap.to(document.getElementById('man'), {
+    gsap.to('#man', {
       scrollTrigger: {
         scrub: 1,
       },
       scale: 0.5,
     });
-    gsap.to(document.getElementById('luna'), {
+    gsap.to('#luna', {
       scrollTrigger: {
         scrub: 1,
       },
       x: -200,
     });
-    gsap.to(document.getElementById('texto'), {
+    gsap.to('#texto', {
       scrollTrigger: {
         scrub: 1,
       },
       y: 500,
     });
   }
+
+  // scrollAnimations() {
+  //   // gsap.to(document.getElementById('bg'), {
+  //   //   scrollTrigger: {
+  //   //     scrub: 1,
+  //   //   },
+  //   //   scale: 1.5,
+  //   // });
+  //   // gsap.to(document.getElementById('man'), {
+  //   //   scrollTrigger: {
+  //   //     scrub: 1,
+  //   //   },
+  //   //   scale: 0.5,
+  //   // });
+  //   // gsap.to(document.getElementById('luna'), {
+  //   //   scrollTrigger: {
+  //   //     scrub: 1,
+  //   //   },
+  //   //   x: -200,
+  //   // });
+  //   // gsap.to(document.getElementById('texto'), {
+  //   //   scrollTrigger: {
+  //   //     scrub: 1,
+  //   //   },
+  //   //   y: 500,
+  //   // });
+  // }
 }
