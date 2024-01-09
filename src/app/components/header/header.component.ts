68de28d092bd4a20d057eca0ac791fbbb11ddc1a
paulina avila <paulina.avila@ids.com.mx>
2024-01-09 12:29:44 -0600

    acomodo header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,5 @@
 // import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-import {
-  Component,
-  ElementRef,
-  OnInit,
-  ViewChild,
-  Renderer2,
-} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
@@ -15,24 +9,22 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  constructor(private renderer: Renderer2) {}
-
-  // @ViewChild('bg', { static: false })
-  // bg!: ElementRef<HTMLImageElement>;
+  constructor() {}
 
   ngOnInit() {
     gsap.registerPlugin(ScrollTrigger);
-    // this.scrollAnimations();
     gsap.to('#bg', {
       scrollTrigger: {
         trigger: '#bg',
-        scrub: 1,
+        start: 'top top',
+        scrub: 0.5,
       },
       scale: 1.5,
     });
     gsap.to('#man', {
       scrollTrigger: {
         trigger: '#man',
+        start: 'top bottom',
         scrub: 1,
       },
       scale: 0.5,
@@ -40,6 +32,7 @@ export class HeaderComponent implements OnInit {
     gsap.to('#luna', {
       scrollTrigger: {
         trigger: '#luna',
+        start: 'top top',
         scrub: 1,
       },
       x: -200,
@@ -47,36 +40,10 @@ export class HeaderComponent implements OnInit {
     gsap.to('#texto', {
       scrollTrigger: {
         trigger: '#texto',
+        start: 'top top',
         scrub: 1,
       },
       y: 500,
     });
   }
-
-  // scrollAnimations() {
-  //   // gsap.to(document.getElementById('bg'), {
-  //   //   scrollTrigger: {
-  //   //     scrub: 1,
-  //   //   },
-  //   //   scale: 1.5,
-  //   // });
-  //   // gsap.to(document.getElementById('man'), {
-  //   //   scrollTrigger: {
-  //   //     scrub: 1,
-  //   //   },
-  //   //   scale: 0.5,
-  //   // });
-  //   // gsap.to(document.getElementById('luna'), {
-  //   //   scrollTrigger: {
-  //   //     scrub: 1,
-  //   //   },
-  //   //   x: -200,
-  //   // });
-  //   // gsap.to(document.getElementById('texto'), {
-  //   //   scrollTrigger: {
-  //   //     scrub: 1,
-  //   //   },
-  //   //   y: 500,
-  //   // });
-  // }
 }
